aca2501387e7afd73dfc1a536fcb50842b89320b
Nick Taylor <nick@nicktaylor.ca>
2019-11-08 16:01:04 -0500

    Add routing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate hyper;
 extern crate jsonwebtoken as jwt;
 #[macro_use] extern crate serde_derive;
+extern crate futures;
 
-use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::{Body, Request, Response, Server, StatusCode, Method};
 use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::service::service_fn;
+use futures::future;
 
 use jwt::{encode, decode, Header, Algorithm, Validation};
 
@@ -17,25 +19,37 @@ struct Claims {
 
 fn main() {
     let addr = ([0, 0, 0, 0], 3000).into();
+    let server = Server::bind(&addr).serve(|| service_fn(route)).map_err(|e| eprintln!("Server error: {}", e));
 
-    let new_svc = || {
-        service_fn_ok(hello_world)
-    };
-
-    let server = Server::bind(&addr).serve(new_svc).map_err(|e| eprintln!("Server error: {}", e));
     hyper::rt::run(server);
 }
 
-fn hello_world(req: Request<Body>) -> Response<Body> {
-    let mut response = Response::new(Body::from(""));
+type FutureResponse = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
+fn route(req: Request<Body>) -> FutureResponse {
+    let mut res = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/authorize") => {
+            authorize(req, &mut res);
+        }
+
+        _ => {
+            *res.status_mut() = StatusCode::NOT_FOUND;
+        }
+    }
+
+    Box::new(future::ok(res))
+}
+
+fn authorize(req: Request<Body>, res: &mut Response<Body>) {
     let encoded_token = match req.headers().get("token") {
         Some(t) =>
             match t.to_str() {
                 Ok(s) => s,
                 Err(_) => {
-                    *response.status_mut() = StatusCode::UNAUTHORIZED;
-                    return response;
+                    *res.status_mut() = StatusCode::UNAUTHORIZED;
+                    return;
                 }
             }
         None => "",
@@ -44,10 +58,8 @@ fn hello_world(req: Request<Body>) -> Response<Body> {
     let token = match decode::<Claims>(encoded_token, "secret".as_ref(), &Validation::default()) {
         Ok(t) => t,
         Err(_) => {
-            *response.status_mut() = StatusCode::UNAUTHORIZED;
-            return response;
+            *res.status_mut() = StatusCode::UNAUTHORIZED;
+            return;
         },
     };
-
-    return response;
 }
